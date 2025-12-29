f7b24f45d20abb4626a296f5ac989563b79a2f09
Yuri Egorov <ysegorov@gmail.com>
2025-12-29 10:40:20 +0000

    ungoogled-chromium: version 143.0.7499.169_2 (rename chromium.sh and chromium.desktop files, update path to binary and install script)

--- srcpkgs/ungoogled-chromium/files/chromium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export CHROME_WRAPPER=/usr/lib/chromium/chromium
-export CHROME_DESKTOP=chromium.desktop
-CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-case $(xbps-uhelper arch) in
-	*-musl) exec /usr/lib/chromium/chromium $CHROME_FLAGS --js-flags="--jitless --wasm_jitless" "$@";;
-	*) exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@";;
-esac
